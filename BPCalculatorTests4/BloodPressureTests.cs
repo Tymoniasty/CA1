7dca625b02d54f045094f2075aac08aedd6e50ef
tym0niasty <tymon.tymoniasty@gmail.com>
2022-11-29 22:48:33 +0000

    added unit testing project - not configured

---
+++ BPCalculatorTests4/BloodPressureTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BPCalculator;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BPCalculator.Tests
+{
+    [TestClass()]
+    public class BloodPressureTests
+    {
+        [TestMethod()]
+        public void GetCategoryTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
