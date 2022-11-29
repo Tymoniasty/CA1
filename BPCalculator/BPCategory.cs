7e14117cb20e134e39c9eb7cc637da4d2762c754
tym0niasty <tymon.tymoniasty@gmail.com>
2022-11-29 22:08:11 +0000

    code amendments to split code into modules

---
+++ BPCalculator/BPCategory.cs
@@ -0,0 +1,11 @@
+﻿namespace BPCalculator
+{
+    // BP categories
+    public enum BPCategory
+    {
+        [Display(Name = "Low Blood Pressure")] Low,
+        [Display(Name = "Ideal Blood Pressure")] Ideal,
+        [Display(Name = "Pre-High Blood Pressure")] PreHigh,
+        [Display(Name = "High Blood Pressure")] High
+    };
+}
--- BPCalculator/BloodPressure.cs
+++ BPCalculator/BloodPressure.cs
@@ -1,14 +1,5 @@
 ﻿namespace BPCalculator
 {
-    // BP categories
-    public enum BPCategory
-    {
-        [Display(Name = "Low Blood Pressure")] Low,
-        [Display(Name = "Ideal Blood Pressure")] Ideal,
-        [Display(Name = "Pre-High Blood Pressure")] PreHigh,
-        [Display(Name = "High Blood Pressure")] High
-    };
-
     public class BloodPressure
     {
         public const int SystolicMin = 70;
@@ -23,32 +14,29 @@
         public int Diastolic { get; set; }                      // mmHG
 
         // calculate BP category
-        public BPCategory Category
+        public BPCategory GetCategory()
         {
-            get
+            //if Systolic between 190(SystolicMax) and 140(inclusive) AND Diastolic between 100(DiastolicMax) and 90(inclusive)
+            if (Systolic <= SystolicMax && Systolic >= 140 && Diastolic <= DiastolicMax && Diastolic >= 90)
+            {  //High result
+                return BPCategory.High;
+            }
+            //if Sysyolic between 120(inclusive) and 140 AND Diastolic between 80(inclusive) and 90
+            else if (Systolic >= 120 && Systolic < 140 && Diastolic >= 80 && Diastolic < 90)
             {
-                //if Systolic between 190(SystolicMax) and 140(inclusive) AND Diastolic between 100(DiastolicMax) and 90(inclusive)
-                if ((Systolic <= SystolicMax && Systolic >= 140) && (Diastolic <= DiastolicMax && Diastolic >= 90)
-                {  //High result
-                    return BPCategory.High;
-                }
-                //if Sysyolic between 120(inclusive) and 140 AND Diastolic between 80(inclusive) and 90
-                else if ((Systolic >= 120 && Systolic < 140) && (Diastolic >= 80 && Diastolic < 90)
-                {
-                    //Pre-high result
-                    return BPCategory.PreHigh;
-                }
-                //if Systolic between 90(inclusive) and 120 AND Diastolic between 60(inclusive) and 80
-                else if ((Systolic >= 90 && Systolic < 120) && (Diastolic >= 60 && Diastolic < 80)
-                {
-                    //Ideal result
-                    return BPCategory.Ideal;
-                }
-                //if Systolic between 70(inclusive) and 90 AND Diastolic between 40(inclusive) and 60
-                else if ((Systolic >= SystolicMin && Systolic < 90) && (Diastolic >= DiastolicMin && Diastolic < 60))
-                {   //Low result
-                    return BPCategory.Low;
-                }
+                //Pre-high result
+                return BPCategory.PreHigh;
+            }
+            //if Systolic between 90(inclusive) and 120 AND Diastolic between 60(inclusive) and 80
+            else if (Systolic >= 90 && Systolic < 120 && Diastolic >= 60 && Diastolic < 80)
+            {
+                //Ideal result
+                return BPCategory.Ideal;
+            }
+            //if Systolic between 70(inclusive) and 90 AND Diastolic between 40(inclusive) and 60
+            else if (Systolic >= SystolicMin && Systolic < 90 && Diastolic >= DiastolicMin && Diastolic < 60)
+            {   //Low result
+                return BPCategory.Low;
             }
         }
     }
