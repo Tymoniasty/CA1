2559083864f2f194359edb07c2a34cec607be678
Tymon Tymoniasty <113236151+tym0niasty@users.noreply.github.com>
2022-12-10 22:57:49 +0000

    Unit Testing

--- BPCalculatorUnitTesting/BloodPressureTests.cs
+++ BPCalculatorUnitTesting/BloodPressureTests.cs
@@ -10,9 +10,33 @@ namespace BPCalculator.Tests
     public class BloodPressureTests
     {
         [TestMethod()]
-        public void GetCategoryTest()
+       
+        public void Test_High()
         {
-            Assert.Fail();
+            //if Systolic = 150 and Diastolic = 95 then it should be 'High' pressure
+            BloodPressure bp = new BloodPressure() { Systolic = 150, Diastolic = 95 };
+            Assert.AreEqual(BPCategory.High, BPCategory);
+        }
+        [TestMethod()]
+        public void Test_Ideal()
+        {
+            //if Systolic = 115 and Diastolic = 70 then it should be 'Ideal' pressure
+            BloodPressure bp = new BloodPressure() { Systolic = 115, Diastolic = 70 };
+            Assert.AreEqual(BPCategory.Ideal, BPCategory);
+        }
+        [TestMethod()]
+        public void Test_Low()
+        {
+            //if Systolic = 80 and Diastolic = 80 then it should be 'Low' pressure
+            BloodPressure bp = new BloodPressure() { Systolic = 80, Diastolic = 50 };
+            Assert.AreEqual(BPCategory.Low, BPCategory);
+        }
+        [TestMethod()]
+        public void Test_PreHigh()
+        {
+            //if Systolic = 125 and Diastolic = 85 then it should be 'PreHigh' pressure
+            BloodPressure bp = new BloodPressure() { Systolic = 125, Diastolic = 85 };
+            Assert.AreEqual(BPCategory.PreHigh, BPCategory);
         }
     }
 }
